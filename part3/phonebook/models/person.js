df305811b3495fa967325bc2d8f8e2cf3133d544
Anuta Kuzmych <a.kusmych@gmail.com>
2026-07-22 23:20:18 +0300

    complete exercise 3.20

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -21,7 +21,17 @@ const personSchema = new mongoose.Schema({
     minlength: [3, "Name must be at least 3 characters long"],
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    minlength: [8, "Number must be at least 8 characters long"],
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number! It should be in the format XX-XXXXXXX or XXX-XXXXXXX`,
+    },
+    required: true,
+  },
 });
 
 personSchema.set("toJSON", {

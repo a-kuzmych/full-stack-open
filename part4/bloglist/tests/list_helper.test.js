20051297298b6345369a22092ea75c6630c95c67
Anuta Kuzmych <a.kusmych@gmail.com>
2026-07-27 00:13:44 +0300

    complete exercise 4.4

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -8,3 +8,47 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+
+describe('total likes', () => {
+  test('of empty list is zero', () => {
+    const blogs = []
+
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const blogs = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~webster/Applets/GoTo.html',
+        likes: 5,
+      },
+    ]
+
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const blogs = [
+      {
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+      },
+      {
+        title: 'Canonical string reduction',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.cs.utexas.edu/users/EWD/ewd06xx/EWD687.Node.html',
+        likes: 12,
+      },
+    ]
+
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 17)
+  })
+})

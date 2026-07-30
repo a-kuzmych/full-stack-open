691c1e1a6165b15644578f563b50a1a7157a8efc
Anuta Kuzmych <a.kusmych@gmail.com>
2026-07-30 23:03:48 +0300

    complete exercise 4.6

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -22,8 +22,37 @@ const favoriteBlog = (blogs) => {
   return favorite
 }
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  const authorCounts = {}
+
+  for (const blog of blogs) {
+    if (authorCounts[blog.author]) {
+      authorCounts[blog.author]++
+    } else {
+      authorCounts[blog.author] = 1
+    }
+  }
+
+  let maxBlogs = 0
+  let mostProlificAuthor = null
+
+  for (const author in authorCounts) {
+    if (authorCounts[author] > maxBlogs) {
+      maxBlogs = authorCounts[author]
+      mostProlificAuthor = author
+    }
+  }
+
+  return { author: mostProlificAuthor, blogs: maxBlogs }
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
 }

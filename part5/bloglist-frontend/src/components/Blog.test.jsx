a1ec42204bcf8afa7be0f60f61ee8ecfeffb919a
Anuta Kuzmych <a.kusmych@gmail.com>
2026-09-01 18:22:21 +0300

    complete exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders title and author, but not url or likes by default', () => {
@@ -20,4 +21,36 @@ test('renders title and author, but not url or likes by default', () => {
   expect(authorElement).toBeInTheDocument()
   expect(urlElement).not.toBeInTheDocument()
   expect(likesElement).not.toBeInTheDocument()
+})
+
+test('clicking the view button shows url and likes', async () => {
+  const blog = {
+    title: 'Test Blog Title', 
+    author: 'Test Author',
+    url: 'http://testurl.com',
+    likes: 5,
+    user: {
+      name: 'Test User',
+      username: 'testuser',
+    },
+  }
+
+  const testActiveUser = {
+    username: 'testuser',
+    name: 'Test User'
+  }
+
+  const mockHandleLike = vi.fn()
+
+  render(<Blog blog={blog} handleLike={mockHandleLike} user={testActiveUser} />)
+
+  const user = userEvent.setup()
+  const viewButton = screen.getByText('view')
+  await user.click(viewButton)
+
+  const urlElement = screen.getByText('http://testurl.com')
+  const likesElement = screen.getByText('likes 5')
+
+  expect(urlElement).toBeInTheDocument()
+  expect(likesElement).toBeInTheDocument()
 })
